f3dfd8697cbd5044caf7b454d2cec561ac02778f
LeeGaoShei <45043985+LeeGaoShei@users.noreply.github.com>
2020-06-01 15:45:50 +0800

    #Updated login to handle exceptions on email and password

--- client/src/views/Pages/LoginPage.jsx
+++ client/src/views/Pages/LoginPage.jsx
@@ -114,7 +114,45 @@ class LoginPage extends React.Component {
     this.refs.notificationAlert.notificationAlert(options);
   }
 
+
+
+  testLogin = () => {
+    console.log("Testing now!")
+    var email = this.state.email;
+    //login["email"] = e.target.value;
+    var emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (emailRex.test(email) && this.state.password.length > 0) {
+
+    } else if (emailRex.test(email) && this.state.password.length <= 0) {
+      this.setState({ message: 'Please enter your password' })
+      throw new Error()
+    } else if (!emailRex.test(email) && this.state.password.length > 0) {
+      this.setState({ message: 'Please enter a correct email format' })
+      throw new Error()
+    } else if (!emailRex.test(email) && this.state.password.length <= 0) {
+      this.setState({ message: 'Please enter a correct email format and password' })
+      throw new Error()
+
+    }
+  }
+
   handleSubmit = (event) => {
+    try {
+      this.testLogin()
+      this.loginViaServer()
+    } catch (err) {
+
+    } finally {
+      setTimeout(() => {
+        this.notify('br', 4)
+      }, 200);
+
+    }
+
+    event.preventDefault();
+  }
+
+  loginViaServer = () => {
     let values = {
       email: this.state.email,
       password: this.state.password
@@ -151,32 +189,11 @@ class LoginPage extends React.Component {
         this.setState({ message: err.message })
         console.log(this.state.message)
       })
-
-    event.preventDefault();
   }
 
-  successAlert() {
-    this.setState({
-      alert: (
-        <SweetAlert
-          success
-          style={{ display: "block", marginTop: "-100px" }}
-          title="Success!"
-          onConfirm={() => this.hideAlert()}
-          onCancel={() => this.hideAlert()}
-        //confirmBtnBsStyle="info"
-        >
-          {this.state.message}
-        </SweetAlert>
-      )
-    });
-  }
 
-  hideAlert() {
-    this.setState({
-      alert: null
-    });
-  }
+
+
 
   render() {
 
