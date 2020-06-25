293a4155d49b3e0c391d3082e23458f459ba2abe
LeeGaoShei <45043985+LeeGaoShei@users.noreply.github.com>
2020-06-25 14:09:10 +0800

    # Removed console logs

--- client/src/views/Pages/LoginPage.jsx
+++ client/src/views/Pages/LoginPage.jsx
@@ -218,7 +218,6 @@ class LoginPage extends React.Component {
       if (response.status === 200) {
 
         var user = response.data.userDetails
-        console.log(user.twoFactorAuth)
 
         if (user.twoFactorAuth) {
           this.setState({ twoFA: true, user })
@@ -237,8 +236,6 @@ class LoginPage extends React.Component {
               this.setState({
                 otp: response.data.otp,
               }, () => { console.log(this.state.otp) })
-
-              console.log("twoFA set to true: " + response.data.otp)
             }
           })
 
@@ -247,7 +244,6 @@ class LoginPage extends React.Component {
           this.notify("tc", 5)
 
           var d = new Date(user.joiningDate)
-          console.log(d.toString());
           var c = new Intl.DateTimeFormat("en-GB", {
             year: "numeric",
             month: "long",
@@ -277,7 +273,7 @@ class LoginPage extends React.Component {
         password: ''
       })
     }).catch((err) => {
-      console.log(err.message)
+
     })
 
   }
@@ -291,7 +287,6 @@ class LoginPage extends React.Component {
 
           var user = this.state.user
           var d = new Date(user.joiningDate)
-          console.log(d.toString());
           var c = new Intl.DateTimeFormat("en-GB", {
             year: "numeric",
             month: "long",
@@ -302,7 +297,6 @@ class LoginPage extends React.Component {
           localStorage.setItem('user', JSON.stringify(user))
 
           this.notify("tc", 5)
-          console.log("Redirecting")
           this.redirect()
 
         })
