631c7c74e1ac5746e61281909285a7dd6e7e38be
GlennTan <e0310963@u.nus.edu>
2020-07-08 17:40:59 +0800

    # added URL address for our ubuntu server for production mode

--- client/src/api-config.js
+++ client/src/api-config.js
@@ -3,7 +3,8 @@ let api;
 if (process.env.NODE_ENV === "development") {
     api = "http://localhost:5001"
 } else if (process.env.NODE_ENV === "production") {
-    api = "http://localhost:5001"
+    //api = "http://localhost:5001"
+    api = "http://172.25.76.183/api"
 }
 
 export { api } 
